8b7f27dffe15173c7bcf2a19cd4a66bd2ac82fec
Filipe Freire <filipefreire@protonmail.com>
2020-07-20 15:25:27 +0100

    Complete all iterations

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    if (this.items.length) {
+      this.items.sort((a, b) => {
+        return a - b;
+      });
+    }
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    const arrClone = [...this.items];
+    const sum = arrClone.reduce((acc, item) => acc + item, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length) {
+      const arrClone = [...this.items];
+      const sum = arrClone.reduce((acc, item) => acc + item, 0);
+      return sum / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
